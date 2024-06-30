644c1d81529c7381d262d3a517c4b77a1801d386
HelmiDev <helmipaty@gmail.com>
2024-06-30 15:19:55 +0100

    Your commit message

---
+++ script.py
@@ -0,0 +1,56 @@
+from asyncio import sleep
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+# Initialize the WebDriver
+
+index=0
+while True:
+    try:
+        print (f"iteration {index+1}")    
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        wait = WebDriverWait(driver, 30)  # Increased timeout to 30 seconds
+        # Open the initial URL
+        driver.get('https://learn.microsoft.com/training/paths/security-copilot-and-ai/?wt.mc_id=studentamb_394481')
+
+        # Click on the "Start" button
+        start_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="start-path"]')))
+        start_button.click()
+
+       
+
+        for i in range(9):
+            try:
+                button1 = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="next-section"]/div/div/p/a')))
+                button1.click()
+                time.sleep(1)
+            except:
+                break
+
+        button1 = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="quiz-choice-2-0"]')))
+        button1.click()
+        button1 = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="quiz-choice-1-0"]')))
+        button1.click()
+        button1 = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="quiz-choice-0-0"]')))
+        button1.click()
+        button1 = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="unit-inner-section"]/form/fieldset/button')))
+        button1.click()
+        button1 = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="quiz-button-holder"]/div/a')))
+        button1.click()
+        time.sleep(1)
+        button1 = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="next-section"]/div/div/p/a')))
+        button1.click()
+        time.sleep(5)
+        print (f"iteration {index+1} success")   
+        driver.quit()
+   
+
+    except:
+        print (f"iteration {index+1} failed")    
+    index+=1    
+
